f94b43029d3313f15f5ed813dbbc4dac82513af2
OF-Abraao <abraaoliveirapro@gmail.com>
2025-07-26 17:17:31 -0300

    feat(add): modulo de adcionar item

--- service/add.ts
+++ service/add.ts
@@ -1,3 +1,41 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as readline from 'readline-sync';
+
 export function IniciaAdd() {
-  console.log('Função de adicionar item ainda não implementada');
+  const caminho = path.join(process.cwd(), 'dados', 'produtos.csv');
+
+  if (!fs.existsSync(caminho)) {
+    console.log('Arquivo CSV não encontrado.');
+    return null;
+  }
+
+  const conteudo = fs.readFileSync(caminho, 'utf8');
+  const linhas = conteudo.trim().split('\n');
+  const dados = linhas.slice(1); // remove cabeçalho
+
+  const nomeInput = readline.question('Nome do item: ').trim().toLowerCase();
+  let nomeExiste = false;
+
+  for (const linha of dados) {
+    const colunas = linha.split(',');
+    const nomeNoArquivo = colunas[0].trim().toLowerCase();
+
+    if (nomeNoArquivo === nomeInput) {
+      nomeExiste = true;
+      break;
+    }
+  }
+
+  if (nomeExiste) {
+    console.log(`Item "${nomeInput}" já existe. Nada foi adicionado.`);
+    return;
+  } else {
+    const peso = readline.questionFloat('Peso (em kg): ');
+    const valor = readline.questionFloat('Valor: ');
+    const quantidade = readline.questionInt('Quantidade: ');
+    const novaLinha = `\n${nomeInput},${peso},${valor},${quantidade}`;
+    fs.appendFileSync(caminho, novaLinha, 'utf8');
+    console.log(`Item "${nomeInput}" adicionado com sucesso!`);
+  }
 }
