3422c4f1116d58b8b634186bf18ecc6231d8c14d
OF-Abraao <abraaoliveirapro@gmail.com>
2025-07-26 17:18:03 -0300

    feat(add): modulo de remover item

--- service/remove.ts
+++ service/remove.ts
@@ -1,3 +1,45 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as readline from 'readline-sync';
+
 export function IniciaRemove() {
-  console.log('Função de remover item ainda não implementada');
+  const caminho = path.join(process.cwd(), 'dados', 'produtos.csv');
+
+  if (!fs.existsSync(caminho)) {
+    console.log('Arquivo CSV não encontrado.');
+    return null;
+  }
+
+  const conteudo = fs.readFileSync(caminho, 'utf8');
+  const linhas = conteudo.trim().split('\n');
+  const dados = linhas.slice(1); // remove cabeçalho
+
+  const nomeInput = readline.question('Nome do item para remover: ').trim().toLowerCase();
+  let nomeExiste = false;
+
+  for (const linha of dados) {
+    const colunas = linha.split(',');
+    const nomeNoArquivo = colunas[0].trim().toLowerCase();
+
+    if (nomeNoArquivo === nomeInput) {
+      nomeExiste = true;
+      break;
+    }
+  }
+
+  if (!nomeExiste) {
+    console.log(`Item "${nomeInput}" não existe. Nada foi removido.`);
+    return;
+  } else {
+    const cabecalho = linhas[0];
+    const novasLinhas = [cabecalho];
+    for (let i = 1; i < linhas.length; i++) {
+      const colunas = linhas[i].split(',');
+      const nomeNoArquivo = colunas[0].trim().toLowerCase();
+      if (nomeNoArquivo !== nomeInput) {
+        novasLinhas.push(linhas[i]);
+      }
+    }  
+    fs.writeFileSync(caminho, novasLinhas.join('\n') + '\n', 'utf8');
+  }
 }
