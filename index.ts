483cb68ac7734b8eec0e84fc1090d649756d2db8
OF-Abraao <abraaoliveirapro@gmail.com>
2025-07-27 16:44:28 -0300

    refactor(index) : refatora o index para assíncrono

--- index.ts
+++ index.ts
@@ -10,61 +10,63 @@ import { IniciaValue } from './metrics/value';
 import { IniciaWeight } from './metrics/weight';
 import * as readline from 'readline-sync';
 
-let executando = true; 
 
-Iniciadatabase();
-while (executando){
-  const opcao = readline.questionInt(
-    "\nQual funcionalidade deseja executar?\n" +
-    "1: Adicionar itens\n" +
-    "2: Remover itens\n" +
-    "3: Verificação de Métricas\n" +
-    "4: Sair\n" +
-    "Digite a opção desejada: "
-  );
-  switch(opcao){
-    case 1:
-      IniciaAdd();
-      break;
-    case 2:
-      IniciaRemove();
-      break;
-    case 3:
-      let analisando = true;
-      while (analisando) {
-        const subOpcao = readline.questionInt(
-          "\nMétricas disponíveis:\n" +
-          "1: Média de valor\n" +
-          "2: Média de peso\n" +
-          "3: Listar itens\n" +
-          "4: Quantidade total de itens\n" +
-          "5: Quantidade de produtos\n" +
-          "6: Valor total\n" +
-          "7: Peso total\n" +
-          "8: Voltar\n" +
-          "Digite a opção desejada: "
-        );
+async function main() {
+  let executando = true; 
 
-        switch (subOpcao) {
-          case 1: IniciaAValue(); break;
-          case 2: IniciaAWeight(); break;
-          case 3: IniciaList(); break;
-          case 4: IniciaQuantItens(); break;
-          case 5: IniciaQuantiProduc(); break;
-          case 6: IniciaValue(); break;
-          case 7: IniciaWeight(); break;
-          case 8: analisando = false; break;
-          default: console.log("Opção inválida."); break;
+  Iniciadatabase();
+  while (executando){
+    const opcao = readline.questionInt(
+      "\nQual funcionalidade deseja executar?\n" +
+      "1: Adicionar itens\n" +
+      "2: Remover itens\n" +
+      "3: Verificação de Métricas\n" +
+      "4: Sair\n" +
+      "Digite a opção desejada: "
+    );
+    switch(opcao){
+      case 1:
+        await IniciaAdd();
+        break;
+      case 2:
+        await IniciaRemove();
+        break;
+      case 3:
+        let analisando = true;
+        while (analisando) {
+          const subOpcao = readline.questionInt(
+            "\nMétricas disponíveis:\n" +
+            "1: Média de valor\n" +
+            "2: Média de peso\n" +
+            "3: Listar itens\n" +
+            "4: Quantidade total de itens\n" +
+            "5: Quantidade de produtos\n" +
+            "6: Valor total\n" +
+            "7: Peso total\n" +
+            "8: Voltar\n" +
+            "Digite a opção desejada: "
+          );
+
+          switch (subOpcao) {
+            case 1: await IniciaAValue(); break;
+            case 2: await IniciaAWeight(); break;
+            case 3: await IniciaList(); break;
+            case 4: await IniciaQuantItens(); break;
+            case 5: await IniciaQuantiProduc(); break;
+            case 6: await IniciaValue(); break;
+            case 7: await IniciaWeight(); break;
+            case 8: analisando = false; break;
+            default: console.log("Opção inválida."); break;
+          }
         }
-      }
-      break;
-    case 4:
-      executando = false;
-      console.log("Encerrando o sistema...");
-      break;
-    default:
-      console.log("Opção inválida.");
+        break;
+      case 4:
+        executando = false;
+        console.log("Encerrando o sistema...");
+        break;
+      default:
+        console.log("Opção inválida.");
+    }
   }
 }
-
-
+main();
